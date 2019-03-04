3b6f82278178886472fe0f949352dc2876220be4
monikakambojofficial96 <monikakambojofficial96@gmail.com>
2019-03-04 15:01:53 +0530

    design a web view

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView=(WebView)findViewById(R.id.webview);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://www.google.com");
+    }
+}
